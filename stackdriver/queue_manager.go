89ab985f6efcf5b888ff163aefd09a2b6e739942
Javier Kohen <jkohen@google.com>
2018-02-28 16:36:46 -0500

    Reset the timer after reading from its channel. Before this change, if the <-timer.C case is entered, the timer isn't reset, but the channel is drained, so if timer.Stop() returns false and L445 tries to read from timer.C, it gets stuck.

--- stackdriver/queue_manager.go
+++ stackdriver/queue_manager.go
@@ -444,13 +444,13 @@ func (s *shards) runShard(i int) {
 			if !timer.Stop() {
 				<-timer.C
 			}
-			timer.Reset(s.qm.cfg.BatchSendDeadline)
 		case <-timer.C:
 			if len(pendingSamples) > 0 {
 				s.sendSamples(client, pendingSamples)
 				pendingSamples = pendingSamples[:0]
 			}
 		}
+		timer.Reset(s.qm.cfg.BatchSendDeadline)
 	}
 }
 
